9c35262d13eb3a54ab7dfd2dd8dcece480786c4e
Tyler Lafferty <tylerlafferty4@gmail.com>
2017-08-29 15:53:44 -0400

    Adding Splash Screen

---
+++ BVInfoSwift/BVInfoSwift/LaunchScreenViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LaunchScreenViewController.swift
+//  BVInfoSwift
+//
+//  Created by Tyler Lafferty on 8/29/17.
+//  Copyright © 2017 Tyler Lafferty. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LaunchScreenViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+}
